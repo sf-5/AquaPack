bc8d492fc37af54904dc48f1e9ff017229ce12b9
sjfishbu <sjfishbu@ncsu.edu>
2024-10-02 22:18:28 -0400

    Initial commit

---
+++ morse_code/src/main.rs
@@ -0,0 +1,53 @@
+use std::fmt::Binary;
+
+fn main() {
+    let s: String = String::from("--.-");
+    if s == ".-" {
+        print!("A");
+    }
+    else if s == "-..." {
+        print!("B");
+    }
+    else if s == "-.-." {
+        print!("C");
+    }
+    else if s == "-.." {
+        print!("D");
+    }
+    else if s == "." {
+        print!("E");
+    }
+    else if s == ".-" {
+        print!("F");
+    }
+    else if s == ".-" {
+        print!("G");
+    }
+    else if s == ".-" {
+        print!("H");
+    }
+    else if s == ".-" {
+        print!("I");
+    }
+    else if s == ".-" {
+        print!("J");
+    }
+    else if s == ".-" {
+        print!("K");
+    }
+    else if s == ".-" {
+        print!("L");
+    }
+    else if s == ".-" {
+        print!("M");
+    }
+    else if s == ".-" {
+        print!("N");
+    }
+    else if s == ".-" {
+        print!("O");
+    }
+    else if s == ".-" {
+        print!("P");
+    }
+}
